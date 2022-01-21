01070e1da1c0d68c4892fcfbb88ca684e788896f
MasseRaphael <masseraphael76@gmail.com>
2022-01-21 11:43:08 +0100

    mise en forme

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,27 @@ import NewTodo from '../components/NewTodo';
 import Todo from '../components/Todo';
 import Notification from '../components/Notification';
 
+const Main = styled.main`
+flex: 1;
+display: flex;
+flex-direction: column;
+jsutify-content: center;
+align-items: center;
+`;
 
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+min-height: 100vh;
+`;
+
+const TodoContainer = styled.div`
+padding: 50px;
+boder: 1px solid black;
+display: flex;
+flex-direction: column;
+min-height: 500px;
+`;
 
 const Home: NextPage = () => {
   const themeMode = useSelector<RootState>((state) => state.theme.mode)
@@ -18,10 +38,22 @@ const Home: NextPage = () => {
   return (
     <ThemeProvider theme={themeMode === "light" ? lightTheme : darkTheme}>
 <GlobalStyles />
-<NavBar />
-<Todo />
-<NewTodo />
-<Notification notification ={notification} />
+<Head>
+  <title>NWS - Todo App with Redux</title>
+</Head>
+
+<Container className='container'>
+  <NavBar />
+  <Main>
+    <TodoContainer>
+      <NewTodo />
+      <Todo />
+    </TodoContainer>
+    {notification.isActive && (
+      <Notification notification={notification} />
+    )}
+  </Main>
+</Container>
     </ThemeProvider>
   
   )
